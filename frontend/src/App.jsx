eca912448d9a9a8cc16cc08e35930d46322499f2
harshmunjal20 <sarlamunjal4@gmail.com>
2026-05-06 13:18:52 +0530

    Frontend Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="text-red-500 text-2xl font-bold">
+      Hello 
+      <h1 className="text-blue-500 text-2xl font-bold">
+        World
+      </h1>
+    </div>
+  )
+}
+
+export default App;
